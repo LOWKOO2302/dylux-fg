9e2cd60d1cbdeb8bff6255246ec031ab854bb419
LOWKOO2302 <121208007+LOWKOO2302@users.noreply.github.com>
2024-02-10 07:48:35 -0500

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 	//const url = 'https://www.youtube.com/watch?v=' + videoId
 	m.react('🎧')
 	let play = `
-	≡ *FG MUSIC*
+	≡ *🤖┃ ᴮᴼᵀ MUSIC*
 ┌──────────────
 ▢ 📌 *Título* : ${title}
 ▢ 📆 *Publicado:* ${ago}
