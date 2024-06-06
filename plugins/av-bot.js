9c2c1348b721ae11073fe4ce6f43d1f97b19893f
LOWKOO2302 <121208007+LOWKOO2302@users.noreply.github.com>
2024-06-06 00:47:58 -0400

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -7,7 +7,7 @@ let av = `./src/mp3/${pickRandom(["criss", "andrea"])}.mp3`
 conn.sendButton(m.chat, `Hola *${name}* \n \nNecesitas ayuda? \n`, fgig, null, [
       ['⦙☰ Menu', '/help'],
       ['⦙☰ Menu 2', '/menu2'],
-      ['⌬ Grupos', '/lowkoogp']
+      ['⌬ Grupos', '/gplowkoo']
     ], m)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
