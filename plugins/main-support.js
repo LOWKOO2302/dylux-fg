52fed0e501e6da356a4b2281c92b9dafc1b1a11f
LOWKOO2302 <121208007+LOWKOO2302@users.noreply.github.com>
2024-06-06 01:33:15 -0400

    Update main-support.js

--- plugins/main-support.js
+++ plugins/main-support.js
@@ -2,27 +2,27 @@
 let handler = async (m, { conn }) => {
 
 m.reply(`
-≡  *🤖┃ ᴮᴼᵀ ┃ SUPPORT*
+≡  *🤖┃ ᴮᴼᵀ ┃ SUPPORT GROUPS LINKS*
 
 ◈ ━━━━━━━━━━━━━━━━━━━━ ◈
-▢ SOLO LINKS Grupo *1*
+▢ ┃𒀯 ⛓️ ᏕᎧᏝᎧ  𒂶  ᏝᎥᏁᏦᏕ  ➊➑⁺  𒀯┃
 https://chat.whatsapp.com/FyEjePGLCOr701GxnvvbQI
 
-▢ ENLACES Y PELICULAS Grupo *2*
+▢ ┃𒀭⛓️𝜩ⲚᏝ𐤠Ꮸ𝜩Ⲋ Dᴱ 𒂶 💬 Ꮃʰᵃᵗˢᴬᵖᵖ Ꮍ Ꭾᵉˡ༏ᶜᵘˡᵃˢ 🍿 𒀭┃
 https://chat.whatsapp.com/HvzT09WBkuG9DIa7e4zMuE
 
-▢ LETS PLAYS JUEGOS APK Grupo *3*
+▢ ┃🖲️⃤🤖 ꤈𝜩Ƭ'𝚂 Ƿ꤈𐤠ⲩ 🎮 𒂶 ᒍꪙ𝜩ᏵⲞⲊ 𒀭 𐤠ᴾᴷˢ ꖾᴬᶜᴷˢ 📲⃤┃
 https://chat.whatsapp.com/Hq2bBBAg3bpBDo1Zrpfw3Y
 
-▢ PELICULAS Y SERIES Grupo *4*
+▢ ┃🎬⃤🍿 Ꭾ𝜩Ꮭ༏ᏨปᏝ𐤠Ⲋ  𒂶  Ⲋ𝜩☈༏𝜩Ⲋ 📽️⃤🖥️ ┃
 https://chat.whatsapp.com/EST7ZYKGBvQ2v4ZSTHIGSd
 
-▢ MUSICAS
+▢ ┃🎙️⃤📲  ☈𝛯Ɗ𝛯ꗟ  𒂶  ꗟ𝙊Ꮸ༏𞋙꤈𝛯ꗟ  📡⃤📺┃
 https://chat.whatsapp.com/GtXAWDlphvw3XHcb7scmdw
 
+▢ ┃😍 🌺 丂ㄒ🍄匚Ҝ乇尺丂 🎭 𒂶 ✨ 𐔣ᵃⁿᵒˢ 𒀭 💓 🤩 ┃
+https://chat.whatsapp.com/JiqP6NTNWbw84kmJKO5U8x
 ◈ ━━━━━━━━━━━━━━━━━━━━ ◈
-▢ STICKERS SANOS
- https://chat.whatsapp.com/JiqP6NTNWbw84kmJKO5U8x
 
 ▢ *Telegram*
 • https://t.me/fgawgp
