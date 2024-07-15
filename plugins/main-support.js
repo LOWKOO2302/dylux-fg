ce295aad3c6417114ea38adcaf5e2aca410a53d6
LOWKOO2302 <121208007+LOWKOO2302@users.noreply.github.com>
2024-07-15 00:53:22 -0400

    Update main-support.js

--- plugins/main-support.js
+++ plugins/main-support.js
@@ -5,31 +5,34 @@ m.reply(`
 ≡  *🤖┃ ᴮᴼᵀ ┃ UNETE A NUESTROS GRUPOS*
 
 ◈ ━━━━━━━━━━━━━━━━━━━━ ◈
-┃ 𒀯  Ⲙ  ⵡ  Ⲡ  Ɗ  Ⲟ  𒂶 𐓈𝛯  𐤠Ƿ𝙊☈Ʈ𝛯Ⲋ   ➊➑⁺ 💬  𒀯 ┃
+*┃ 𒀯  Ⲙ  ⵡ  Ⲡ  Ɗ  Ⲟ  𒂶 𐓈𝛯  𐤠Ƿ𝙊☈Ʈ𝛯Ⲋ   ➊➑⁺ 💬  𒀯 ┃*
 ▢ 
-https://chat.whatsapp.com/Ke1Aghdz0QVII6kyXSQv7E
+*https://chat.whatsapp.com/Ke1Aghdz0QVII6kyXSQv7E*
 
-▢ ┃𒀯 ⛓️ ᏕᎧᏝᎧ  𒂶  ᏝᎥᏁᏦᏕ  ➊➑⁺  𒀯┃
-https://chat.whatsapp.com/FyEjePGLCOr701GxnvvbQI
+▢ *┃𒀯 ⛓️ ᏕᎧᏝᎧ  𒂶  ᏝᎥᏁᏦᏕ  ➊➑⁺  𒀯┃*
+*https://chat.whatsapp.com/FyEjePGLCOr701GxnvvbQI*
 
-▢ ┃𒀭⛓️𝜩ⲚᏝ𐤠Ꮸ𝜩Ⲋ Dᴱ 𒂶 💬 Ꮃʰᵃᵗˢᴬᵖᵖ Ꮍ Ꭾᵉˡ༏ᶜᵘˡᵃˢ 🍿 𒀭┃
-https://chat.whatsapp.com/HvzT09WBkuG9DIa7e4zMuE
+▢ *┃𒀭⛓️𝜩ⲚᏝ𐤠Ꮸ𝜩Ⲋ Dᴱ 𒂶 💬 Ꮃʰᵃᵗˢᴬᵖᵖ Ꮍ Ꭾᵉˡ༏ᶜᵘˡᵃˢ 🍿 𒀭┃*
+*https://chat.whatsapp.com/HvzT09WBkuG9DIa7e4zMuE*
 
-▢ ┃🖲️⃤🤖 ꤈𝜩Ƭ'𝚂 Ƿ꤈𐤠ⲩ 🎮 𒂶 ᒍꪙ𝜩ᏵⲞⲊ 𒀭 𐤠ᴾᴷˢ ꖾᴬᶜᴷˢ 📲⃤┃
-https://chat.whatsapp.com/Hq2bBBAg3bpBDo1Zrpfw3Y
+▢ *┃🖲️⃤🤖 ꤈𝜩Ƭ'𝚂 Ƿ꤈𐤠ⲩ 🎮 𒂶 ᒍꪙ𝜩ᏵⲞⲊ 𒀭 𐤠ᴾᴷˢ ꖾᴬᶜᴷˢ 📲⃤┃*
+*https://chat.whatsapp.com/Hq2bBBAg3bpBDo1Zrpfw3Y*
 
-▢ ┃🎬⃤🍿 Ꭾ𝜩Ꮭ༏ᏨปᏝ𐤠Ⲋ  𒂶  Ⲋ𝜩☈༏𝜩Ⲋ 📽️⃤🖥️ ┃
-https://chat.whatsapp.com/EST7ZYKGBvQ2v4ZSTHIGSd
+▢ *┃🎬⃤🍿 Ꭾ𝜩Ꮭ༏ᏨปᏝ𐤠Ⲋ  𒂶  Ⲋ𝜩☈༏𝜩Ⲋ 📽️⃤🖥️ ┃*
+*https://chat.whatsapp.com/EST7ZYKGBvQ2v4ZSTHIGSd*
 
-▢ ┃✨💓⃤Ɯ𐤠꤈꤈Ꭾ𐤠Ꭾ𝜩☈'Ⲋ 𒂶 Ꝼ𐔖ꤚⱰ𐔖Ⲋ Ɗᵉ Ꭾᵃⁿᵗᵃˡˡᵃˢ 🌌⃤✨┃
+▢ *┃✨ 🎧⃤ ⏪⏸⏩ Ⲙ 𞤋 Ꚃ ༏ ꓚ 𐤠 Ꚃ  𒂶  ꪜ ⁱᵈᵉᵒˢ Ʈⁱᵏᵗᵒᵏ 📺⃤ ✨┃*
 https://chat.whatsapp.com/GtXAWDlphvw3XHcb7scmdw
 
-▢ ┃😍 🌺 丂ㄒ🍄匚Ҝ乇尺丂 🎭 𒂶 ✨ 𐔣ᵃⁿᵒˢ 𒀭 💓 🤩 ┃
-https://chat.whatsapp.com/JiqP6NTNWbw84kmJKO5U8x
+▢ *┃😍 🌺 丂ㄒ🍄匚Ҝ乇尺丂 🎭 𒂶 ✨ 𐔣ᵃⁿᵒˢ 𒀭 💓 🤩 ┃*
+*https://chat.whatsapp.com/JiqP6NTNWbw84kmJKO5U8x*
+
+▢ *┃✨💓⃤Ɯ𐤠꤈꤈Ꭾ𐤠Ꭾ𝜩☈'Ⲋ 𒂶 Ꝼ𐔖ꤚⱰ𐔖Ⲋ Ɗᵉ Ꭾᵃⁿᵗᵃˡˡᵃˢ 🌌⃤✨┃*
+*https://chat.whatsapp.com/FTfdyf392efG8Zazl6zrMC*
 ◈ ━━━━━━━━━━━━━━━━━━━━ ◈
 
 ▢ *ADMIN*
-• ᏝᎧᏇᏦᎧᎧ ➘ 👽`)
+*• ᏝᎧᏇᏦᎧᎧ ➘ 👽*`)
 
 
 }
