add2b45cdec62c1a73f2d782cad7213b3ddd25a5
LOWKOO2302 <121208007+LOWKOO2302@users.noreply.github.com>
2024-08-28 03:43:08 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['17745108093', '🤖┃ᴮᴼᵀ', true],
+  ['17745198093', '🤖┃ᴮᴼᵀ', true],
   ['17745190032'], 
   ['17744205538'] 
 ] //Numeros de owner 
