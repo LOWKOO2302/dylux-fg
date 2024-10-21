a6ac6ce26ba7c4796879e6b02b98cba080181e05
BOT2302 <121208007+LOWKOO2302@users.noreply.github.com>
2024-10-20 21:22:08 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5492622271736'], 
-  ['5492622271736'] 
+  ['16145174095', 'FG98', true],
+  ['17745198093'], 
+  ['17744205538'] 
 ] //Numeros de owner 
 
 global.mods = ['573125484672'] 
