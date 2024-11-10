210b6ff966cd607bc22072fc6de9023a6a5dd266
BOT2302 <121208007+LOWKOO2302@users.noreply.github.com>
2024-11-09 22:45:22 -0500

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -12,7 +12,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 Tiempo Activo ${muptime}\n\n ┃ 💎  By FG98`
+		let bio = `\n┃🟢 ᗩᗪᗰIᑎ'Տ 𒂶 ᑕᕼᗩT'Տ ᑕOᑎTᖇOᒪ 24/7 🟢  ┃\n\n 🤖┃ ᴮᴼᵀ
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
